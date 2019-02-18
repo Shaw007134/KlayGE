252bc91b5ed70c1253a14a5ba42c96eec17c34a9
Minmin Gong <gongminmin@msn.com>
2019-02-17 20:10:56 -0800

    KlayGE: OpenGLES: Refactor OGLESShaderObject to have shader stage objects

--- KlayGE/Plugins/Include/KlayGE/OpenGLES/OGLESShaderObject.hpp
+++ KlayGE/Plugins/Include/KlayGE/OpenGLES/OGLESShaderObject.hpp
@@ -19,8 +19,209 @@
 #include <KlayGE/RenderLayout.hpp>
 #include <KlayGE/ShaderObject.hpp>
 
+namespace DXBC2GLSL
+{
+	class DXBC2GLSL;
+}
+
 namespace KlayGE
 {
+	class OGLESShaderStageObject
+	{
+	public:
+		OGLESShaderStageObject(ShaderObject::ShaderType stage, GLenum gl_shader_type);
+		virtual ~OGLESShaderStageObject();
+
+		void StreamIn(RenderEffect const& effect, std::array<uint32_t, ShaderObject::ST_NumShaderTypes> const& shader_desc_ids,
+			std::vector<uint8_t> const& native_shader_block);
+		void StreamOut(std::ostream& os);
+		void AttachShader(RenderEffect const& effect, RenderTechnique const& tech, RenderPass const& pass,
+			std::array<uint32_t, ShaderObject::ST_NumShaderTypes> const& shader_desc_ids);
+
+		bool Validate() const
+		{
+			return is_validate_;
+		}
+
+		std::string const& GlslSource() const
+		{
+			return glsl_src_;
+		}
+
+		std::string const& ShaderFuncName() const
+		{
+			return shader_func_name_;
+		}
+
+		std::vector<std::string> const& PNames() const
+		{
+			return pnames_;
+		}
+
+		std::vector<std::string> const& GlslResNames() const
+		{
+			return glsl_res_names_;
+		}
+
+		std::vector<std::pair<std::string, std::string>> const& TexSamplerPairs() const
+		{
+			return tex_sampler_pairs_;
+		}
+
+		GLuint GlShader() const
+		{
+			return gl_shader_;
+		}
+
+		virtual uint32_t DsPartitioning() const
+		{
+			return 0;
+		}
+		virtual uint32_t DsOutputPrimitive() const
+		{
+			return 0;
+		}
+
+	private:
+		virtual std::string_view GetShaderProfile(RenderEffect const& effect, uint32_t shader_desc_id) const = 0;
+		void CreateHwShader();
+
+		virtual void StageSpecificStreamIn(std::istream& native_shader_stream)
+		{
+			KFL_UNUSED(native_shader_stream);
+		}
+		virtual void StageSpecificStreamOut(std::ostream& os)
+		{
+			KFL_UNUSED(os);
+		}
+		virtual void StageSpecificAttachShader(DXBC2GLSL::DXBC2GLSL const& dxbc2glsl)
+		{
+			KFL_UNUSED(dxbc2glsl);
+		}
+
+	protected:
+		const ShaderObject::ShaderType stage_;
+		const GLenum gl_shader_type_;
+
+		bool is_validate_;
+
+		std::string shader_func_name_;
+		std::string glsl_src_;
+		std::vector<std::string> pnames_;
+		std::vector<std::string> glsl_res_names_;
+
+		std::vector<std::pair<std::string, std::string>> tex_sampler_pairs_;
+
+		GLuint gl_shader_ = 0;
+	};
+
+	class OGLESVertexShaderStageObject : public OGLESShaderStageObject
+	{
+	public:
+		OGLESVertexShaderStageObject();
+
+		std::vector<VertexElementUsage> const& Usages() const
+		{
+			return usages_;
+		}
+		std::vector<uint8_t> const& UsageIndices() const
+		{
+			return usage_indices_;
+		}
+		std::vector<std::string> const& GlslAttribNames() const
+		{
+			return glsl_attrib_names_;
+		}
+
+	private:
+		std::string_view GetShaderProfile(RenderEffect const& effect, uint32_t shader_desc_id) const override;
+
+		void StageSpecificStreamIn(std::istream& native_shader_stream) override;
+		void StageSpecificStreamOut(std::ostream& os) override;
+		void StageSpecificAttachShader(DXBC2GLSL::DXBC2GLSL const& dxbc2glsl) override;
+
+	private:
+		std::vector<VertexElementUsage> usages_;
+		std::vector<uint8_t> usage_indices_;
+		std::vector<std::string> glsl_attrib_names_;
+	};
+
+	class OGLESPixelShaderStageObject : public OGLESShaderStageObject
+	{
+	public:
+		OGLESPixelShaderStageObject();
+
+	private:
+		std::string_view GetShaderProfile(RenderEffect const& effect, uint32_t shader_desc_id) const override;
+	};
+
+	class OGLESGeometryShaderStageObject : public OGLESShaderStageObject
+	{
+	public:
+		OGLESGeometryShaderStageObject();
+
+	private:
+		std::string_view GetShaderProfile(RenderEffect const& effect, uint32_t shader_desc_id) const override;
+	};
+
+	class OGLESComputeShaderStageObject : public OGLESShaderStageObject
+	{
+	public:
+		OGLESComputeShaderStageObject();
+
+	private:
+		std::string_view GetShaderProfile(RenderEffect const& effect, uint32_t shader_desc_id) const override;
+	};
+
+	class OGLESHullShaderStageObject : public OGLESShaderStageObject
+	{
+	public:
+		OGLESHullShaderStageObject();
+
+#if KLAYGE_IS_DEV_PLATFORM
+		uint32_t DsPartitioning() const override
+		{
+			return ds_partitioning_;
+		}
+		uint32_t DsOutputPrimitive() const override
+		{
+			return ds_output_primitive_;
+		}
+#endif
+
+	private:
+		std::string_view GetShaderProfile(RenderEffect const& effect, uint32_t shader_desc_id) const override;
+
+#if KLAYGE_IS_DEV_PLATFORM
+		void StageSpecificAttachShader(DXBC2GLSL::DXBC2GLSL const& dxbc2glsl) override;
+#endif
+
+#if KLAYGE_IS_DEV_PLATFORM
+	private:
+		uint32_t ds_partitioning_ = 0;
+		uint32_t ds_output_primitive_ = 0;
+#endif
+	};
+
+	class OGLESDomainShaderStageObject : public OGLESShaderStageObject
+	{
+	public:
+		OGLESDomainShaderStageObject();
+
+#if KLAYGE_IS_DEV_PLATFORM
+		void DsParameters(uint32_t partitioning, uint32_t output_primitive);
+#endif
+
+	private:
+		std::string_view GetShaderProfile(RenderEffect const& effect, uint32_t shader_desc_id) const override;
+
+#if KLAYGE_IS_DEV_PLATFORM
+	private:
+		uint32_t ds_partitioning_ = 0;
+		uint32_t ds_output_primitive_ = 0;
+#endif
+	};
+
 	struct TextureBind
 	{
 		ShaderResourceViewPtr buff_srv;
@@ -63,22 +264,11 @@ namespace KlayGE
 	private:
 		struct OGLESShaderObjectTemplate
 		{
-			OGLESShaderObjectTemplate();
-
 			GLenum glsl_bin_format_;
 			std::vector<uint8_t> glsl_bin_program_;
-			std::array<std::string, ST_NumShaderTypes> shader_func_names_;
-			std::array<std::shared_ptr<std::string>, ST_NumShaderTypes> glsl_srcs_;
-			std::array<std::shared_ptr<std::vector<std::string>>, ST_NumShaderTypes> pnames_;
-			std::array<std::shared_ptr<std::vector<std::string>>, ST_NumShaderTypes> glsl_res_names_;
-			std::vector<VertexElementUsage> vs_usages_;
-			std::vector<uint8_t> vs_usage_indices_;
-			std::vector<std::string> glsl_vs_attrib_names_;
+			std::array<std::shared_ptr<OGLESShaderStageObject>, ST_NumShaderTypes> shader_stages_;
 			std::vector<std::string> glsl_tfb_varyings_;
 			bool tfb_separate_attribs_;
-#if KLAYGE_IS_DEV_PLATFORM
-			uint32_t ds_partitioning_, ds_output_primitive_;
-#endif
 		};
 
 		struct ParameterBind
@@ -90,15 +280,16 @@ namespace KlayGE
 			std::function<void()> func;
 		};
 
-		void AttachGLSL(uint32_t type);
+	public:
+		explicit OGLESShaderObject(std::shared_ptr<OGLESShaderObjectTemplate> const & so_template);
+
+	private:
+		void CreateShaderStage(ShaderType stage);
+		void AppendTexSamplerBinds(
+			ShaderType stage, RenderEffect const& effect, std::vector<std::pair<std::string, std::string>> const& tex_sampler_pairs);
 		void LinkGLSL();
 		void AttachUBOs(RenderEffect const & effect);
 		void FillTFBVaryings(ShaderDesc const & sd);
-		void PrintGLSLError(ShaderType type, std::string_view info);
-		void PrintGLSLErrorAtLine(std::string const & glsl, int err_line);
-
-	public:
-		explicit OGLESShaderObject(std::shared_ptr<OGLESShaderObjectTemplate> const & so_template);
 
 	private:
 		std::shared_ptr<OGLESShaderObjectTemplate> so_template_;
